48ff8c3d898cd6f18e2e1d1240e0343bbfc73400
ayaz7964 <ayazhussain.bssef22@iba-suk.edu.pk>
2025-07-01 16:11:25 +0500

    updated backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,77 +1,30 @@
-// const express = require ("express")
-// const app = express()
-// const mongoose = require ("mongoose")
-// const routes = require("./Routes/routes")
+const express = require ("express")
+const app = express()
+const mongoose = require ("mongoose")
+const routes = require("./Routes/routes")
 
-// const dbURL = "mongodb+srv://ayaz:1234@cluster0.65rzobs.mongodb.net/CiviAnalytics?retryWrites=true&w=majority&appName=Cluster0"
+const dbURL = "mongodb+srv://ayaz:1234@cluster0.65rzobs.mongodb.net/CiviAnalytics?retryWrites=true&w=majority&appName=Cluster0"
 
-// mongoose.connect(dbURL).then(()=>{
-//     console.log('Server Stated Port Number 4000 and Database Connected ')
-//     app.listen(4000)
-// }).catch((err)=>{
-//     console.log(err)
-// })
-// const cors = require('cors');
-// app.use(cors({
-//   origin: 'https://satisnation.vercel.app'
-// }));
+mongoose.connect(dbURL).then(()=>{
+    console.log('Server Stated Port Number 4000 and Database Connected ')
+    app.listen(4000)
+}).catch((err)=>{
+    console.log(err)
+})
 
 
-// // Middleware to parse JSON bodies
-// app.use(express.json()); 
+// Middleware to parse JSON bodies
+app.use(express.json()); 
 
-// app.use((req, res, next) => {
-//    console.log(`${req.method} request for '${req.url}'`);
-//     next();
-// });
-
-// app.use("/api",routes);
-// app.use('/',(req,res)=>{
-// res.json({msg:"Working fine"})
-// })
-
-
-
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./Routes/routes");
-const cors = require("cors");
-
-// Create app
-const app = express();
-
-// MongoDB URL
-const dbURL = "mongodb+srv://ayaz:1234@cluster0.65rzobs.mongodb.net/CiviAnalytics?retryWrites=true&w=majority&appName=Cluster0";
-
-// Middleware
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://satisnation.vercel.app'],
-  credentials: true
-}));
-app.use(express.json());
 app.use((req, res, next) => {
-  console.log(`${req.method} request for '${req.url}'`);
-  next();
+   console.log(`${req.method} request for '${req.url}'`);
+    next();
 });
-app.use("/api", routes);
-app.get("/", (req, res) => res.json({ msg: "Backend root is working" }));
 
-// ✅ Check if running locally (via dev command or script)
-if (require.main === module) {
-  // Connect DB and start server
-  mongoose.connect(dbURL).then(() => {
-    console.log("MongoDB Connected");
-    app.listen(4000, () => console.log("Server running on http://localhost:4000"));
-  }).catch(err => console.error("MongoDB Error:", err));
-}
+app.use("/api",routes);
+app.use('/',(req,res)=>{
+res.json({msg:"Working fine"})
+})
 
-// ✅ Export for Vercel (serverless)
-module.exports = async (req, res) => {
-  if (mongoose.connection.readyState !== 1) {
-    await mongoose.connect(dbURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-  }
-  return app(req, res); // handle request with express app
-};
+
+
