80bc875038ede8382dde6c979208edeaa078d15e
ayaz7964 <ayazhussain.bssef22@iba-suk.edu.pk>
2025-07-01 16:28:22 +0500

    cors added in server

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,14 @@ const routes = require("./Routes/routes")
 
 const dbURL = "mongodb+srv://ayaz:1234@cluster0.65rzobs.mongodb.net/CiviAnalytics?retryWrites=true&w=majority&appName=Cluster0"
 
+const cors = require("cors");
+
+app.use(cors({
+  origin: "https://satisnation.vercel.app", // Your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 mongoose.connect(dbURL).then(()=>{
     console.log('Server Stated Port Number 4000 and Database Connected ')
     app.listen(4000)
