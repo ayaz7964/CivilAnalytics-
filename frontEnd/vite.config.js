171d57952d52fb6b9a71d8986fa950912dbf3b76
ayaz7964 <ayazhussain.bssef22@iba-suk.edu.pk>
2025-07-01 17:01:23 +0500

    vercel updated

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:4000' || 'https://satisnation-api.vercel.app'
+      '/api': 'http://localhost:4000' 
 
     }
   }
